be77448d95848c86cdef0989528eaeaa05404340
PK7107 <patryk070108@wp.pl>
2025-01-15 13:41:49 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 const App = () => {
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
   return (
-    <>
-      <p>Elo 320</p>    
-    </>
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Hello {handleNameChange()}!
+        </p>
+      </header>
+    </div>
   );
 }
 
